a18be6a6206f27c5963b6fc5e994a2c59c2ce178
Timothee Glörfeld <timothee.gloerfeld@exxeta.com>
2022-05-13 20:24:18 +0200

    add Respawn

--- Tests.fs
+++ Tests.fs
@@ -6,11 +6,31 @@ open FsHttp
 open System.Text.Json
 open System.Text.Json.Serialization
 open System.Net
+open Respawn
+open Npgsql
 
 GlobalConfig.Json.defaultJsonSerializerOptions <-
-    let options = JsonSerializerOptions()
+    let options = new JsonSerializerOptions()
     options.Converters.Add(JsonFSharpConverter())
     options
+
+type RespawnFixture() =
+    member this.Reset() =
+        let checkpoint = new Checkpoint(DbAdapter = DbAdapter.Postgres)
+
+        use conn =
+            new NpgsqlConnection(
+                "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=mysecretpassword;"
+            )
+
+        conn.Open()
+
+        checkpoint.Reset conn
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    interface IDisposable with
+        member this.Dispose() = this.Reset()
 
 type Todo =
     { Id: Guid
@@ -19,13 +39,43 @@ type Todo =
       CreatedAt: DateTime
       UpdatedAt: DateTime }
 
-[<Fact>]
-let ``Get Todos`` () =
-    let response =
-        http { GET "http://localhost:5228/todos" }
+type IntegrationTests(respawnFixture: RespawnFixture) =
+    [<Fact>]
+    member _.``Get Todos``() =
+        let response =
+            http { GET "http://localhost:5228/todos" }
+            |> Request.send
+
+        let todos = response |> Response.deserializeJson<List<Todo>>
+
+        Assert.Equal(HttpStatusCode.OK, response.statusCode)
+        Assert.Empty(todos)
+
+
+    [<Fact>]
+    member _.``Post Todos``() =
+        http {
+            POST "http://localhost:5228/todos"
+            body
+
+            json
+                """
+            {
+                "text": "new todo"
+            }
+            """
+        }
         |> Request.send
+        |> ignore
 
-    let todos = response |> Response.deserializeJson<List<Todo>>
+        let response =
+            http { GET "http://localhost:5228/todos" }
+            |> Request.send
 
-    Assert.Equal(HttpStatusCode.OK, response.statusCode)
-    Assert.NotEmpty(todos)
+        let todos = response |> Response.deserializeJson<List<Todo>>
+
+        Assert.Equal(HttpStatusCode.OK, response.statusCode)
+        Assert.Equal(1, todos.Length)
+        Assert.NotEmpty(todos)
+
+    interface IClassFixture<RespawnFixture>
